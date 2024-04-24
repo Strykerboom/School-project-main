68094cd272e5d5603501da6380a4d59ad25d8916
Phineas Pompei <phineas.pompei@gmail.com>
2024-04-24 06:30:55 -0400

    Initial Commmit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+const moveButton = document.getElementById('moveButton');
+
+// Add click event listener to the button
+moveButton.addEventListener('click', function() {
+    // Redirect to page2.html when the button is clicked
+    window.location.href = 'page.html';
+});
